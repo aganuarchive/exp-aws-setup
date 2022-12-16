d22d83cefa5dd530131efa72e8abde9d7bb4985c
aganuarchive <92853156+aganuarchive@users.noreply.github.com>
2022-12-16 11:17:37 +0000

    Add files via upload

--- setupconfig/installsetup.sh
+++ setupconfig/installsetup.sh
@@ -1,5 +1,5 @@
-aws cloudformation delete-stack --stack-name myexprole 
-aws cloudformation create-stack --stack-name myexprole --template-body ExpPolicyRole --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation wait stack-delete-complete --stack-name myexprole 
+aws cloudformation wait stack-create-complete --stack-name myexprole --template-body ExpPolicyRole --capabilities CAPABILITY_NAMED_IAM
 
-aws cloudformation delete-stack --stack-name myexpcodebuild 
-aws cloudformation create-stack --stack-name myexpcodebuild --template-body expcodebuild 
+aws cloudformation wait stack-delete-complete --stack-name myexpcodebuild 
+aws cloudformation wait stack-create-complete --stack-name myexpcodebuild --template-body expcodebuild 
