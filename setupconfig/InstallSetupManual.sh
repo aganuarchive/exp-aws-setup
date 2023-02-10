25acddb86cf006dc68ef7b047dfbfb711e2b7746
aganuarchive <92853156+aganuarchive@users.noreply.github.com>
2023-02-10 22:46:54 +0000

    Add files via upload

--- setupconfig/InstallSetupManual.sh
+++ setupconfig/InstallSetupManual.sh
@@ -1,17 +1,17 @@
 -- github token :  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
 -- AWS CLI commands
 aws cloudformation delete-stack --stack-name myexprole  
-aws cloudformation create-stack --stack-name myexprole --template-body ./ExpPolicyRole --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation create-stack --stack-name myexprole --template-body file://./ExpPolicyRole --capabilities CAPABILITY_NAMED_IAM
 
 aws cloudformation delete-stack --stack-name mytestdynamo  
-aws cloudformation create-stack --stack-name mytestdynamo --template-body ./ExpDynamoScript  
+aws cloudformation create-stack --stack-name mytestdynamo --template-body file://./ExpDynamoScript  
 
 aws cloudformation delete-stack --stack-name myexps3NS  
-aws cloudformation create-stack --stack-name myexps3NS --template-body ./ExpS3Script   --parameters ParameterKey=BUCKETNAME,ParameterValue=expjan2023store
+aws cloudformation create-stack --stack-name myexps3NS --template-body file://./ExpS3Script   --parameters ParameterKey=BUCKETNAME,ParameterValue=expjan2023store
 aws dynamodb put-item --table-name RESTABLE --item "{\"RESKEY\" : { \"S\":\"S3BUCKET\"}, \"RESNAME\" : {\"S\" : \"expjan2023store2\"}, \"RESTYPE\" : {\"S\" : \"S3\"}}"
 
-aws cloudformation create-stack --stack-name myexplambdaCustomFnSNS --template-body ./ExpLambdaCustomFnSNS   
-aws cloudformation create-stack --stack-name myexpcodebuild --template-body ./ExpCodeBuild    
+aws cloudformation create-stack --stack-name myexplambdaCustomFnSNS --template-body file://./ExpLambdaCustomFnSNS   
+aws cloudformation create-stack --stack-name myexpcodebuild --template-body file://./ExpCodeBuild    
 
 aws codebuild start-build --project-name CB-LAMBDALAYERS
 aws codebuild start-build --project-name CB-UPDATEFNLAYERS
@@ -26,39 +26,39 @@ aws s3 cp stacks s3://expjan2023store2/stacks --recursive
 ---------------------------------
 
 aws cloudformation delete-stack --stack-name myexplambda  
-aws cloudformation create-stack --stack-name myexplambda --template-body ./ExpLambdasWithNS   
+aws cloudformation create-stack --stack-name myexplambda --template-body file://./ExpLambdasWithNS   
 
 aws cloudformation delete-stack --stack-name myexpcodebuildNS  
-aws cloudformation create-stack --stack-name myexpcodebuildNS --template-body ./ExpCodeBuildWithNS    
+aws cloudformation create-stack --stack-name myexpcodebuildNS --template-body file://./ExpCodeBuildWithNS    
 
 aws codebuild start-build --project-name CB-PROCESSSHOPFILE
 aws codebuild start-build --project-name CB-REGISTERSHOPFILE
 aws codebuild start-build --project-name CB-PROCESSONEFILE
 aws codebuild start-build --project-name CB-PROCESSTRNQ
 
-aws cloudformation update-stack --stack-name myexps3NS --template-body ./ExpS3Script-2   --parameters ParameterKey=BUCKETNAME,ParameterValue=expjan2023store2
-aws cloudformation update-stack --stack-name myexps3NS --template-body ./ExpS3Script-3   --parameters ParameterKey=BUCKETNAME,ParameterValue=expjan2023store2
+aws cloudformation update-stack --stack-name myexps3NS --template-body file://./ExpS3Script-2   --parameters ParameterKey=BUCKETNAME,ParameterValue=expjan2023store2
+aws cloudformation update-stack --stack-name myexps3NS --template-body file://./ExpS3Script-3   --parameters ParameterKey=BUCKETNAME,ParameterValue=expjan2023store2
 
 aws cloudformation delete-stack --stack-name myexpqueue  
-aws cloudformation create-stack --stack-name myexpqueue --template-body ./ExpQueueNS    
+aws cloudformation create-stack --stack-name myexpqueue --template-body file://./ExpQueueNS    
 
 aws cloudformation delete-stack --stack-name myexpscheduler  
-aws cloudformation create-stack --stack-name myexpscheduler --template-body ./ExpSchedulerWithNS    
+aws cloudformation create-stack --stack-name myexpscheduler --template-body file://./ExpSchedulerWithNS    
 
 
 aws cloudformation delete-stack --stack-name myexpsns  
-aws cloudformation create-stack --stack-name myexpsns --template-body ./ExpSNS    
+aws cloudformation create-stack --stack-name myexpsns --template-body file://./ExpSNS    
 
 aws cloudformation delete-stack --stack-name myexprds  
-aws cloudformation create-stack --stack-name myexprds --template-body ./ExpRDS    
+aws cloudformation create-stack --stack-name myexprds --template-body file://./ExpRDS    
 
 
 aws cloudformation delete-stack --stack-name myexpapi  
-aws cloudformation create-stack --stack-name myexpapi --template-body ./ExpApi    
-aws cloudformation update-stack --stack-name myexpapi --template-body ./ExpApi    
+aws cloudformation create-stack --stack-name myexpapi --template-body file://./ExpApi    
+aws cloudformation update-stack --stack-name myexpapi --template-body file://./ExpApi    
 
 aws cloudformation delete-stack --stack-name myexpapifnp  
-aws cloudformation create-stack --stack-name myexpapifnp --template-body ./ExpApiFnPermission    
+aws cloudformation create-stack --stack-name myexpapifnp --template-body file://./ExpApiFnPermission    
 
 
 
@@ -70,7 +70,7 @@ aws cloudformation create-stack --stack-name myexpapifnp --template-body ./ExpAp
 
 --------------Obsolete
 aws cloudformation delete-stack --stack-name myexplambdarole  
-aws cloudformation create-stack --stack-name myexplambdarole --template-body ./ExpLambdaRoleScript   --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation create-stack --stack-name myexplambdarole --template-body file://./ExpLambdaRoleScript   --capabilities CAPABILITY_NAMED_IAM
 
 aws cloudformation delete-stack --stack-name myexpec2  
-aws cloudformation create-stack --stack-name myexpec2 --template-body ./ExpEC2   
+aws cloudformation create-stack --stack-name myexpec2 --template-body file://./ExpEC2   
---
+++ setupconfig/processDynamoDBConfig.py
@@ -0,0 +1,49 @@
+import json
+import boto3
+import exputils
+
+def processData(data):
+    for d in data:
+        print(d['Table'])
+        print(d['Data'])
+        if (d['Table'] == "RESTABLE"):
+            processResTableData(d['Table'], d['Data'])
+        else:
+            processTableData(d['Table'], d['Data'])
+
+def processTableData(tablename, data):
+#    tablename = data['Dynamodb'][0]['Table']
+    records = data
+    #dynamodb = boto3.resource('dynamodb', aws_access_key_id='AKIAVBRTAL5UYA7MKFVH',
+    #                      aws_secret_access_key='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', region_name='us-east-1')
+    dynamodb = boto3.resource('dynamodb')
+
+    for rec in records :
+        print(rec)
+        fieldname = rec['FIELDTAG']
+        fieldvalue = rec['FIELDNAME']
+        print( fieldname + " " + fieldvalue)
+        l_table = dynamodb.Table(tablename)
+        l_table.put_item(Item={'FIELDTAG':fieldname,'FIELDNAME':fieldvalue})
+
+def processResTableData(tablename, data):
+    records = data
+    #dynamodb = boto3.resource('dynamodb', aws_access_key_id='AKIAVBRTAL5UYA7MKFVH',
+    #                      aws_secret_access_key='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', region_name='us-east-1')
+    dynamodb = boto3.resource('dynamodb')
+
+    for rec in records :
+        print(rec)
+        fieldname = rec['RESKEY']
+        fieldvalue = rec['RESNAME']
+        fieldtype = rec['RESTYPE']
+        print( fieldname + " " + fieldvalue)
+        l_table = dynamodb.Table(tablename)
+        l_table.put_item(Item={"RESKEY":fieldname,"RESNAME":fieldvalue,"RESTYPE": fieldtype})
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    f = open('C:\Abhay\Personal\AWS\AbgSept2022\Scripts\DynamoDB-table-data.json')
+    data = json.load(f)
+    processData(data['Dynamodb'])
