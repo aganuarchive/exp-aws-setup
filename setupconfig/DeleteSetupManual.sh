e81a325ccfec1c130634380581b17c6460accf5e
GANU Abhay <abhay.ganu@soprabanking.com>
2023-02-27 16:55:13 +0000

    changes

---
+++ setupconfig/DeleteSetupManual.sh
@@ -0,0 +1,15 @@
+aws cloudformation delete-stack --stack-name myexpapifnp  
+aws cloudformation delete-stack --stack-name myexpapi  
+aws cloudformation delete-stack --stack-name myexprds  
+aws cloudformation delete-stack --stack-name myexpscheduler  
+aws cloudformation delete-stack --stack-name myexpsns  
+aws cloudformation delete-stack --stack-name myexpqueue  
+aws cloudformation delete-stack --stack-name myexpcodebuildNS  
+aws cloudformation delete-stack --stack-name myexplambda  
+aws cloudformation delete-stack --stack-name myexpcodebuild 
+aws cloudformation delete-stack --stack-name myexplambdaCustomFnSNS 
+aws cloudformation delete-stack --stack-name myexps3NS  
+aws cloudformation delete-stack --stack-name mytestdynamo  
+aws cloudformation delete-stack --stack-name myexprole  
+
+
--- setupconfig/InstallSetupManual.sh
+++ setupconfig/InstallSetupManual.sh
@@ -6,9 +6,11 @@ aws cloudformation create-stack --stack-name myexprole --template-body file://./
 aws cloudformation delete-stack --stack-name mytestdynamo  
 aws cloudformation create-stack --stack-name mytestdynamo --template-body file://./ExpDynamoScript  
 
+python processDynamoDBConfig.py
+
 aws cloudformation delete-stack --stack-name myexps3NS  
-aws cloudformation create-stack --stack-name myexps3NS --template-body file://./ExpS3Script   --parameters ParameterKey=BUCKETNAME,ParameterValue=expjan2023store
-aws dynamodb put-item --table-name RESTABLE --item "{\"RESKEY\" : { \"S\":\"S3BUCKET\"}, \"RESNAME\" : {\"S\" : \"expjan2023store2\"}, \"RESTYPE\" : {\"S\" : \"S3\"}}"
+aws cloudformation create-stack --stack-name myexps3NS --template-body file://./ExpS3Script   --parameters ParameterKey=BUCKETNAME,ParameterValue=expfeb2023store --capabilities CAPABILITY_AUTO_EXPAND
+aws dynamodb put-item --table-name RESTABLE --item "{\"RESKEY\" : { \"S\":\"S3BUCKET\"}, \"RESNAME\" : {\"S\" : \"expfeb2023store\"}, \"RESTYPE\" : {\"S\" : \"S3\"}}"
 
 aws cloudformation create-stack --stack-name myexplambdaCustomFnSNS --template-body file://./ExpLambdaCustomFnSNS   
 aws cloudformation create-stack --stack-name myexpcodebuild --template-body file://./ExpCodeBuild    
@@ -19,9 +21,7 @@ aws codebuild start-build --project-name CB-CUSTOMFNSNS
 
 aws lambda invoke --function-name updateFnLayers fn.log
 
-aws s3 cp stacks s3://expjan2023store2/stacks --recursive
-
-
+aws s3 cp stacks s3://expfeb2023store/stacks --recursive
 
 ---------------------------------
 
@@ -36,22 +36,26 @@ aws codebuild start-build --project-name CB-REGISTERSHOPFILE
 aws codebuild start-build --project-name CB-PROCESSONEFILE
 aws codebuild start-build --project-name CB-PROCESSTRNQ
 
-aws cloudformation update-stack --stack-name myexps3NS --template-body file://./ExpS3Script-2   --parameters ParameterKey=BUCKETNAME,ParameterValue=expjan2023store2
-aws cloudformation update-stack --stack-name myexps3NS --template-body file://./ExpS3Script-3   --parameters ParameterKey=BUCKETNAME,ParameterValue=expjan2023store2
+#aws cloudformation update-stack --stack-name myexps3NS --template-body file://./ExpS3Script-2   --parameters ParameterKey=BUCKETNAME,ParameterValue=expfeb2023store
+#aws cloudformation update-stack --stack-name myexps3NS --template-body file://./ExpS3Script-3   --parameters ParameterKey=BUCKETNAME,ParameterValue=expfeb2023store
+aws cloudformation create-stack --stack-name myexps3NS1 --template-body file://./ExpS3Script-1   --capabilities CAPABILITY_AUTO_EXPAND
 
 aws cloudformation delete-stack --stack-name myexpqueue  
 aws cloudformation create-stack --stack-name myexpqueue --template-body file://./ExpQueueNS    
+
+aws cloudformation delete-stack --stack-name myexpsns  
+aws cloudformation create-stack --stack-name myexpsns --template-body file://./ExpSNS    
+aws cloudformation create-stack --stack-name myexpsnsPOLICY --template-body file://./ExpSNSPolicy    
 
 aws cloudformation delete-stack --stack-name myexpscheduler  
 aws cloudformation create-stack --stack-name myexpscheduler --template-body file://./ExpSchedulerWithNS    
 
 
-aws cloudformation delete-stack --stack-name myexpsns  
-aws cloudformation create-stack --stack-name myexpsns --template-body file://./ExpSNS    
 
 aws cloudformation delete-stack --stack-name myexprds  
 aws cloudformation create-stack --stack-name myexprds --template-body file://./ExpRDS    
-
+aws cloudformation update-stack --stack-name myexprds --template-body file://./ExpRDS    
+python -c "import processDynamoDBConfig ; processDynamoDBConfig.updateDBEndpoint()"
 
 aws cloudformation delete-stack --stack-name myexpapi  
 aws cloudformation create-stack --stack-name myexpapi --template-body file://./ExpApi    
@@ -60,6 +64,14 @@ aws cloudformation update-stack --stack-name myexpapi --template-body file://./E
 aws cloudformation delete-stack --stack-name myexpapifnp  
 aws cloudformation create-stack --stack-name myexpapifnp --template-body file://./ExpApiFnPermission    
 
+
+aws codebuild start-build --project-name CB-GETITEM
+aws codebuild start-build --project-name CB-GETLOCATIONSMOBILE
+aws codebuild start-build --project-name CB-GETITEMSHOPRATES
+aws codebuild start-build --project-name CB-GETFILTERS
+aws codebuild start-build --project-name CB-UPDATEITEM
+
+aws lambda invoke --function-name updateFnLayers fn.log
 
 
 
@@ -74,3 +86,8 @@ aws cloudformation create-stack --stack-name myexplambdarole --template-body fil
 
 aws cloudformation delete-stack --stack-name myexpec2  
 aws cloudformation create-stack --stack-name myexpec2 --template-body file://./ExpEC2   
+
+aws cloudformation create-stack --stack-name myexpmacro --template-body file://./Macro   --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_NAMED_IAM CAPABILITY_IAM
+aws cloudformation create-stack --stack-name myexplambda2 --template-body file://./ExpLambda2   --capabilities CAPABILITY_AUTO_EXPAND
+
+
