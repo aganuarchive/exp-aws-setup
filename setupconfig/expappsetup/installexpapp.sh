028bf8a4f2f8dce1e58add345f855aebe90d8bd9
GANU Abhay <abhay.ganu@soprabanking.com>
2023-02-28 17:07:20 +0000

    sh modified

--- setupconfig/expappsetup/installexpapp.sh
+++ setupconfig/expappsetup/installexpapp.sh
@@ -1,11 +1,12 @@
 sudo apt update
-sudo apt install openjdk-8-jdk
+sudo apt install -y openjdk-8-jdk
 sudo mkdir /opt/tomcat
-sudo tar xzvf apache-tomcat-10.0.13.tar.gz -C /opt/tomcat --strip-components=1
+cd /home/ubuntu/expapp/exp-aws-setup/setupconfig/expappsetup
+sudo tar xzvf apache-tomcat-8.5.85.tar.gz -C /opt/tomcat --strip-components=1
 sudo groupadd tomcat
 sudo useradd -s /bin/false -g tomcat -d /opt/tomcat tomcat
-chown -R tomcat: /opt/tomcat
-chmod -R 755 /opt/tomcat
+sudo chown -R tomcat: /opt/tomcat
+sudo chmod -R 755 /opt/tomcat
 sudo cp tomcat.service /etc/systemd/system/tomcat.service
 sudo systemctl daemon-reload
 sudo systemctl start tomcat
@@ -14,5 +15,6 @@ sudo cp manager-context.xml /opt/tomcat/webapps/manager/META-INF/context.xml
 sudo cp host-manager-context.xml /opt/tomcat/webapps/host-manager/META-INF/context.xml
 unzip expenseswebapp 
 sudo mkdir /opt/tomcat/webapps/expenseswebapp
-sudo cd /opt/tomcat/webapps/expenseswebapp
-sudo cp -rf expenseswebapp/* .
+sudo cp -rf expenseswebapp/* /opt/tomcat/webapps/expenseswebapp/.
+sudo chown -R tomcat: /opt/tomcat
+sudo systemctl restart tomcat
