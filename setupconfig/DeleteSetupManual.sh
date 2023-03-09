a6dc5d63a504fcb0b33dbdf985ade9461a4d3c7f
aganuarchive <92853156+aganuarchive@users.noreply.github.com>
2023-03-09 20:22:39 +0000

    Add files via upload

--- setupconfig/DeleteSetupManual.sh
+++ setupconfig/DeleteSetupManual.sh
@@ -1,15 +1,38 @@
 aws cloudformation delete-stack --stack-name myexpapifnp  
+aws cloudformation wait stack-delete-complete --stack-name myexpapifnp 
+
 aws cloudformation delete-stack --stack-name myexpapi  
+aws cloudformation wait stack-delete-complete --stack-name myexpapi
+
 aws cloudformation delete-stack --stack-name myexprds  
+aws cloudformation wait stack-delete-complete --stack-name myexprds
+
 aws cloudformation delete-stack --stack-name myexpscheduler  
+aws cloudformation wait stack-delete-complete --stack-name myexpscheduler
+
 aws cloudformation delete-stack --stack-name myexpsns  
+aws cloudformation wait stack-delete-complete --stack-name myexpsns
+
 aws cloudformation delete-stack --stack-name myexpqueue  
+aws cloudformation wait stack-delete-complete --stack-name myexpqueue
+
 aws cloudformation delete-stack --stack-name myexpcodebuildNS  
+aws cloudformation wait stack-delete-complete --stack-name myexpcodebuildNS
+
 aws cloudformation delete-stack --stack-name myexplambda  
+aws cloudformation wait stack-delete-complete --stack-name myexplambda
+
 aws cloudformation delete-stack --stack-name myexpcodebuild 
+aws cloudformation wait stack-delete-complete --stack-name myexpcodebuild
+
 aws cloudformation delete-stack --stack-name myexplambdaCustomFnSNS 
+aws cloudformation wait stack-delete-complete --stack-name myexplambdaCustomFnSNS
+
 aws cloudformation delete-stack --stack-name myexps3NS  
+aws cloudformation wait stack-delete-complete --stack-name myexps3NS
+
 aws cloudformation delete-stack --stack-name mytestdynamo  
+aws cloudformation wait stack-delete-complete --stack-name mytestdynamo
+
 aws cloudformation delete-stack --stack-name myexprole  
-
-
+aws cloudformation wait stack-delete-complete --stack-name myexprole
