cdb9c26875c8c27c3f8121e4dd4d02182ae122c2
aganuarchive <92853156+aganuarchive@users.noreply.github.com>
2023-03-14 16:40:36 +0000

    Add files via upload

--- setupconfig/importRdsBackupFromS3.sh
+++ setupconfig/importRdsBackupFromS3.sh
@@ -4,5 +4,5 @@ sudo sed -i 's/SET @@SESSION.SQL_/-- SET @@SESSION.SQL_/g' exprdsbackup.sql
 sudo sed -i 's/SET @@GLOBAL.GTID_/-- SET @@GLOBAL.GTID_/g' exprdsbackup.sql
 sudo sed -i 's/SET @@SESSION.SQL_LOG/-- SET @@SESSION.SQL_LOG/g' exprdsbackup.sql
 sudo sed -i "s/DEFINER='admin'@'%'/ /g" exprdsbackup.sql
-EXPORT DBURL=`python3.8 -c "import processDynamoDBConfig ; processDynamoDBConfig.getDBEndpoint()"`
-mysql -h $DBURL-u admin -p < /home/ubuntu/expbackup/exprdsbackup.sql
+export DBURL=`python3.8 -c "import processDynamoDBConfig ; processDynamoDBConfig.getDBEndpoint()"`
+mysql -h $DBURL admin -p < /home/ubuntu/expbackup/exprdsbackup.sql
