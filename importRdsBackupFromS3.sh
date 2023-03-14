e372953be0612156dadd2ad7475f4bb1ef0996fb
aganuarchive <92853156+aganuarchive@users.noreply.github.com>
2023-03-14 16:52:04 +0000

    Delete importRdsBackupFromS3.sh

--- importRdsBackupFromS3.sh
+++
@@ -1,8 +0,0 @@
-aws s3 cp s3://expjune2022store/expbackup/exprdsbackup.sql /home/ubuntu/expbackup/exprdsbackup.sql --acl bucket-owner-full-control
-sudo sed -i 's/SET @MYSQLDUMP_TEMP_/-- SET @MYSQLDUMP_TEMP_/g' /home/ubuntu/expbackup/exprdsbackup.sql
-sudo sed -i 's/SET @@SESSION.SQL_/-- SET @@SESSION.SQL_/g' /home/ubuntu/expbackup/exprdsbackup.sql
-sudo sed -i 's/SET @@GLOBAL.GTID_/-- SET @@GLOBAL.GTID_/g' /home/ubuntu/expbackup/exprdsbackup.sql
-sudo sed -i 's/SET @@SESSION.SQL_LOG/-- SET @@SESSION.SQL_LOG/g' /home/ubuntu/expbackup/exprdsbackup.sql
-sudo sed -i "s/DEFINER='admin'@'%'/ /g" /home/ubuntu/expbackup/exprdsbackup.sql
-export DBURL=`python3.8 -c "import processDynamoDBConfig ; processDynamoDBConfig.getDBEndpoint()"`
-mysql -h $DBURL admin -p < /home/ubuntu/expbackup/exprdsbackup.sql
